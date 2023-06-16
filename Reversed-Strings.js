dc2f9e6ea042d1af3b71847704c958a56dce26b1
Lagutina Svetlana <lagutinasvetlana@hotmail.com>
2023-06-16 23:03:33 +0300

    added solution

---
+++ Reversed-Strings.js
@@ -0,0 +1,13 @@
+// Reversed Strings
+
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// -STRINGS,
+// -FUNDAMENTALS
+
+function solution(str){
+    return str.split("").reverse().join("");
+  }
